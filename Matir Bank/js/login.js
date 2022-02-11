023ce7b9f85d553e7ac25068c10992c39cfa7ec1
Sadat Shahriar Bari <shahriardipro38@gmail.com>
2022-02-11 19:44:33 +0600

    test

--- Matir Bank/js/login.js
+++ Matir Bank/js/login.js
@@ -6,7 +6,7 @@ function credentialsCheck(email, password) {
   if (email == 'test@gmail.com' && password == '12345') {
     window.location.href = 'bank.html';
   } else {
-    console.log('login failed');
+    console.log('login failed ');
   }
 }
 
