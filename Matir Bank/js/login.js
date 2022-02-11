240dace61d34c904b77a6b0793e2b26a054b9616
Sadat Shahriar Bari <shahriardipro38@gmail.com>
2022-02-11 19:41:41 +0600

    Matri bank login done

---
+++ Matir Bank/js/login.js
@@ -0,0 +1,17 @@
+const loginButton = document.getElementById('loginButton');
+
+loginButton.addEventListener('click', login);
+
+function credentialsCheck(email, password) {
+  if (email == 'test@gmail.com' && password == '12345') {
+    window.location.href = 'bank.html';
+  } else {
+    console.log('login failed');
+  }
+}
+
+function login() {
+  const emailAdress = document.getElementById('email');
+  const password = document.getElementById('password');
+  credentialsCheck(emailAdress.value, password.value);
+}
